f1dd3911b99d97dcf6306641122077180f2eb6e7
Matt Burke <spraints@gmail.com>
2021-01-22 11:50:58 -0500

    no rails, gonna use probot

--- app/controllers/home_controller.rb
+++
@@ -1,7 +0,0 @@
-class HomeController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: :callback
-
-  def callback
-    @auth_hash = request.env["omniauth.auth"]
-  end
-end
